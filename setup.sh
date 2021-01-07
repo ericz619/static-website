9a7a258a9b578528bc18fe526efffd5d0ecb20e2
ericz1232 <64095845+ericz1232@users.noreply.github.com>
2021-01-07 17:23:43 -0500

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -22,16 +22,18 @@ sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 
 echo "INSTALLING NODEJS"
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash
-nvm install node
-node -e "console.log('Running Node.js ' + process.version)"
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.9/install.sh | bash
+echo 'export NVM_DIR="/home/ubuntu/.nvm"' >> /home/ubuntu/.bashrc
+echo '[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm' >> /home/ubuntu/.bashrc
+# Dot source the files to ensure that variables are available within the current shell
+. /home/ubuntu/.nvm/nvm.sh
+. /home/ubuntu/.profile
+. /home/ubuntu/.bashrc
+# Install NVM, NPM, Node.JS & Grunt
+nvm install --lts
+nvm ls
 
 echo "DOWNLOADING REPO"
-git clone https://github.com/ericz1232/static-website.git 
-    && cd static-website 
-    && npm install
-    && sudo docker build -t ericz99/static-website:latest . --no-cache
-    && sudo docker -d -p 80:8080 ericz99/static-website:latest
-
+git clone https://github.com/ericz1232/static-website.git && cd static-website && npm install && sudo docker build -t ericz99/static-website:latest . --no-cache && sudo docker -d -p 80:8080 ericz99/static-website:latest
 private_ip=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
 curl "http://$private_ip.com"
